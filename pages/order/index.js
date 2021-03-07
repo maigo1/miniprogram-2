98ad4d53ebe73b4b1dd31b4b1cadca7a4a2d50fa
夏涛 <xiatao@xiataodeMacBook-Pro.local>
2021-03-07 22:39:21 +0900

    get order api data

--- pages/order/index.js
+++ pages/order/index.js
@@ -1,10 +1,21 @@
 // pages/order/index.js
-Page({
 
+import { request } from "../../request/index.js";
+import regeneratorRuntime from "../../lib/runtime/runtime";
+Page({
   /**
-   * 页面的初始数据
+   * 1 页面被打开的时候 onShow
+   *  0 onShow 不同于onLoad 无法在形参上接收 options参数 
+   *  0.5 判断缓存中有没有token
+   *    1 没有 跳转到授权页面
+   *    2 有 直接往下进行
+   *  1 获取url上的参数type
+   *  2 根据type 去发送请求获取订单数据
+   *  3 渲染页面
+   * 2 点击不同的标题 重新发送请求来获取和渲染
    */
   data: {
+    orders:[],
     tabs:[
       {
         id:0,
@@ -29,12 +40,40 @@ Page({
     ]
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
+onShow(options){
 
-  },
+
+
+  // 1 获取当前的小程序的页面栈-数组 长度最大是10 页面
+  let pages =  getCurrentPages();
+  // 2 数组中 索引最大的页面就是当前页面
+  let currentPage=pages[pages.length-1];
+  // 3 获取url上的type参数
+  const {type}=currentPage.options;
+  this.getOrders(type);
+  wx.setStorageSync('token', 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOjIzLCJpYXQiOjE1NjQ3MzAwNzksImV4cCI6MTAwMTU2NDczMDA3OH0.YPt-XeLnjV-_1ITaXGY2FhxmCe4NvXuRnRB8OMCfnPo');
+    //1 判断缓存中有没有token
+    const token=wx.getStorageSync("token");
+    //2 判断
+    if(!token){
+      wx.navigateTo({
+        url: '/pages/auth/index'
+      });
+      return;
+    }
+ 
+},
+// 获取订单列表的方法
+async getOrders(type){
+
+  //请求头参数未封装
+  const token=wx.getStorageSync("token");
+  const header = {Authorization:token};
+const res=await request({url:"/my/orders/all",data:{type},header});
+this.setData({
+  orders:res.orders
+})
+},
 
   //标题点击事件  
   handleTabsItemChange(e){
